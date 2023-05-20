3c73f0468d25f87b1ea9e451175523eaa9fe947a
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-20 13:45:59 +0300

    Add files via upload

---
+++ src/socket/socket-client.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { io, Socket } from 'socket.io-client';
+
+@Injectable()
+export class SocketClient implements OnModuleInit {
+    public socketClient: Socket;
+
+    constructor() {
+        this.socketClient = io("http://localhost:5433");
+    }
+    onModuleInit() {
+       this.registetConsumerEvents();
+    }
+
+    private registetConsumerEvents() {
+     // this.socketClient.emit('newMessage', { msg: 'hey there!' });
+        this.socketClient.on('connect', () => {
+            console.log("Connected to Gateway");
+        });
+        this.socketClient.on('onMessage', (payload: any) => {
+            console.log(payload)
+        });
+    }
+}
